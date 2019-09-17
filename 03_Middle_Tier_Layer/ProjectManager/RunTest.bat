e81983545364a546d870e237bee7dc9dbc17646d
Tamoghna <tamoghno2431@gmail.com>
2019-09-17 03:53:21 -0700

    Nunit Test results checked in

--- 03_Middle_Tier_Layer/ProjectManager/RunTest.bat
+++ 03_Middle_Tier_Layer/ProjectManager/RunTest.bat
@@ -4,4 +4,3 @@ echo AutoFrame!
 CD "C:\Users\Tamoghna_Choudhury_210125_FSEFinalAssessment\FSE_Final_210125\03_Middle_Tier_Layer\ProjectManager\packages\NUnit.ConsoleRunner.3.10.0\tools"
 start nunit3-console.exe
 nunit3-console ..\..\..\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
-
